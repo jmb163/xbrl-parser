dc60845cf06fa4f53ebfe89865a053a013abeaec
Jonathan M. Brooks <jmb163@miami.edu>
2020-02-13 18:32:53 -0500

    Made the report getter slightly more useful, in the future I'll add some more stuff for building complete financial reports out of 10-Q reports over a range of time

--- reports_getter.py
+++ reports_getter.py
@@ -26,13 +26,26 @@ if __name__ == '__main__':
 			data_files = tag
 	rows = data_files.find_all('a')
 	files = []
+	group_tags = soup.find('div', {'class':"formContent"}).find_all('div')
+	fdate = None
+
+	for i in range(len(group_tags)):
+		if group_tags[i].text == 'Filing Date':
+			fdate = group_tags[i+1].text
+			break
+	fdate = fdate.replace('-', '')
 	for row in rows:
 		files.append(row.attrs['href'])
 	files = [base+x for x in files]
 	ticker = files[0].split(sep='/')[-1].split(sep='-')[0]
-	outbase = './' + ticker + 'data/'
+	outbase = os.getcwd() + '/' + fdate + ticker
+	if not os.path.exists(outbase):
+		os.mkdir(outbase)
+
+	os.chdir(outbase)
+	outbase += '/'
 	for file in files:
-		outname = outbase + file.split(sep='/')[-1]
+		outname = file.split(sep='/')[-1]
 		req = ses.get(file)
 		with open(outname, mode='w') as f:
 			f.write(req.text)
